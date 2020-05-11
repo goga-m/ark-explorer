fd60e7c7dd995ad3e5d5194d63df2d27ffc04774
goga-m <goga.mamoulasvili@gmail.com>
2020-05-12 00:44:29 +0300

    refactor: use static functions on network controller

--- app/Http/Controllers/SelectedNetwork.php
+++ app/Http/Controllers/SelectedNetwork.php
@@ -9,74 +9,76 @@ use Illuminate\Http\Request;
  * selected network name (Mainnet, Devnet).
  *
  * Stores chain/network values in session.
- *
  */
 class SelectedNetwork extends Controller
 {
-    protected $defaultNetwork = 'Mainnet';
-
-    protected $networks;
-
     /**
-     * Instantiate a new controller instance.
+     * Set default selected network in session
+     * if not set.
      *
      * @return void
      */
-    public function __construct()
+    public static function setDefaultIfNotExists()
     {
-
-      // Network info (urls), hardcoded.
-      $this->networks = collect([
-        [ 'name' => 'Mainnet', 'url' => 'https://explorer.ark.io/api'],
-        [ 'name' => 'Devnet', 'url' => 'https://dexplorer.ark.io/api']
-      ]);
-
-    }
-    /**
-     * Set default selected network in session
-     * if not exists.
-     * @return void
-     */
-    public function setDefaultIfNotExists()
-    {
+        $defaultNetwork = 'Mainnet';
         if (!session()->has('selectedNetwork')) {
-          session()->put('selectedNetwork', $this->defaultNetwork);
+            session()->put('selectedNetwork', $defaultNetwork);
         }
     }
 
     /**
      * Get the name only of the selected network.
+     *
      * @return string
      */
-    public function getName()
+    public static function getName()
     {
-        return session()->get('selectedNetwork');
+        $defaultNetwork = 'Mainnet';
+        $selected = session()->get('selectedNetwork');
+
+        if($selected) return $selected;
+        else return $defaultNetwork;
     }
 
     /**
      * Set the selectedNetwork.
      * Set only if $network name is contained in available networks.
      *
-     * @param integer $networkName Mainnet/Devnet
+     * @param string $networkName Mainnet/Devnet
      * @return void
      */
-    public function set($networkName)
+    public static function set($networkName)
     {
-      if($this->networks->contains('name', $networkName)) {
-        session()->put('selectedNetwork', $networkName);
-      }
+        if(self::getNetworks()->contains('name', $networkName)) {
+            session()->put('selectedNetwork', $networkName);
+        }
     }
 
     /**
-     * Get the url of the selected network (for API requests).
+     * Get the url of the selected network.
+     *
      * @return string
      */
-    public function getUrl()
+    public static function getUrl()
     {
         $selected = session()->get('selectedNetwork');
-        return $this->networks
-                    ->where('name', '===', $selected)
-                    ->pluck('url')
-                    ->first();
+        return self::getNetworks()
+            ->where('name', '===', $selected)
+            ->pluck('url')
+            ->first();
     }
+
+    /**
+     * Get the available (used) networks.
+     *
+     * @return mixed
+     */
+    private static function getNetworks()
+    {
+        return collect([
+            [ 'name' => 'Mainnet', 'url' => 'https://explorer.ark.io/api'],
+            [ 'name' => 'Devnet', 'url' => 'https://dexplorer.ark.io/api']
+        ]);
+    }
+
 }
